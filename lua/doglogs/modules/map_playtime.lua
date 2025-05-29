957655a6ad0abc4cd8245e3a85835f6d7bb0adf5
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2025-05-29 18:00:09 -0500

    Use files to track data through crashes

--- lua/doglogs/modules/map_playtime.lua
+++ lua/doglogs/modules/map_playtime.lua
@@ -1,7 +1,20 @@
-local message = "Lua shutdown! The map was %s, with a session time of %d seconds."
+local message = "DogLogs_MapPlayime: The previous map was %s, with a session time of %d seconds."
+local fileName = "cfc_doglogs_map_playtime.json"
 local startTime = os.time()
 
-hook.Add( "ShutDown", "DogLogs_MapPlaytime", function()
-    local log = string.format( message, game.GetMap(), math.Round( os.time() - startTime ) )
+-- Log the previous session's data, and remove the file.
+if file.Exists( fileName, "DATA" ) then
+    local info = util.JSONToTable( file.Read( fileName, "DATA" ) )
+    local log = string.format( message, info.map, math.Round( info.session_time ) )
     DogLogs.Log( log )
-end )
+
+    file.Delete( fileName, "DATA" )
+end
+
+-- Write to a file on shutdown and every minute, so crashes don't lose data.
+local function updateTracker()
+    file.Write( fileName, util.TableToJSON( { map = game.GetMap(), session_time = os.time() - startTime } ) )
+end
+
+timer.Create( "DogLogs_MapPlaytime", 60, 1, updateTracker )
+hook.Add( "ShutDown", "DogLogs_MapPlaytime", updateTracker )
