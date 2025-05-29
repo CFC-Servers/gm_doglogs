735c143b04f901759c05a6dadf9955d2f9982c87
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2025-05-29 17:38:49 -0500

    Use os.time()

--- lua/doglogs/modules/map_playtime.lua
+++ lua/doglogs/modules/map_playtime.lua
@@ -1,6 +1,7 @@
 local message = "Lua shutdown! The map was %s, with a session time of %d seconds."
+local startTime = os.time()
 
 hook.Add( "ShutDown", "DogLogs_MapPlaytime", function()
-    local log = string.format( message, game.GetMap(), math.Round( RealTime() ) )
+    local log = string.format( message, game.GetMap(), math.Round( os.time() - startTime ) )
     DogLogs.Log( log )
 end )
