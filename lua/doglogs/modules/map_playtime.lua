f40c7c9153d51d1cc98686dd395a4d2a29b57e33
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2025-05-30 10:28:32 -0500

    Print directly as JSON

--- lua/doglogs/modules/map_playtime.lua
+++ lua/doglogs/modules/map_playtime.lua
@@ -1,19 +1,21 @@
-local message = "DogLogs_MapPlayime: The previous map was %s, with a session time of %d seconds."
 local fileName = "cfc_doglogs_map_playtime.json"
 local startTime = os.time()
 
 -- Log the previous session's data, and remove the file.
 if file.Exists( fileName, "DATA" ) then
-    local info = util.JSONToTable( file.Read( fileName, "DATA" ) )
-    local log = string.format( message, info.map, math.Round( info.session_time ) )
-    DogLogs.Log( log )
+    DogLogs.Log( file.Read( fileName, "DATA" ) )
 
     file.Delete( fileName, "DATA" )
 end
 
 -- Write to a file on shutdown and every minute, so crashes don't lose data.
 local function updateTracker()
-    file.Write( fileName, util.TableToJSON( { map = game.GetMap(), session_time = os.time() - startTime } ) )
+    file.Write( fileName, util.TableToJSON( {
+        map_playtime = {
+            map = game.GetMap(),
+            time = os.time() - startTime,
+        }
+    } ) )
 end
 
 timer.Create( "DogLogs_MapPlaytime", 60, 1, updateTracker )
