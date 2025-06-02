7a84b7d899609a79c20f292886763c0fd313f686
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2025-06-02 09:50:44 -0500

    Collect median, mean, etc for some player stats

--- lua/doglogs/modules/ping.lua
+++ lua/doglogs/modules/ping.lua
@@ -1,14 +1,58 @@
 local startTime = os.time()
+local perPlayerStats = {
+    packet_loss = function( ply ) return ply:PacketLoss() end,
+    ping = function( ply ) return ply:Ping() end,
+}
 
 local function ping()
+    local plys = player.GetHumans()
+    local plyCount = #plys
+    local pingData = {
+        received = os.time(),
+        uptime = SysTime(),
+        session_time = os.time() - startTime,
+        player_count = plyCount,
+        map = game.GetMap(),
+    }
+
+    -- Collect statistical values for each player stat.
+    for key, func in pairs( perPlayerStats ) do
+        local tbl = {}
+        pingData[key] = tbl
+
+        -- Avoid divide by zero.
+        if plyCount == 0 then
+            tbl.mean = 0
+            tbl.median = 0
+            tbl.q1 = 0
+            tbl.q3 = 0
+            tbl.min = 0
+            tbl.max = 0
+
+            continue
+        end
+
+        local valTotal = 0
+        local vals = {}
+
+        for i, ply in ipairs( plys ) do
+            local val = func( ply )
+            valTotal = valTotal + val
+            vals[i] = val
+        end
+
+        table.sort( vals )
+
+        tbl.mean = valTotal / plyCount
+        tbl.median = vals[math.ceil( plyCount * 0.5 )]
+        tbl.q1 = vals[math.ceil( plyCount * 0.25 )]
+        tbl.q3 = vals[math.ceil( plyCount * 0.75 )]
+        tbl.min = vals[1]
+        tbl.max = vals[plyCount]
+    end
+
     print( util.TableToJSON( {
-        doglogs_ping = {
-            received = os.time(),
-            uptime = SysTime(),
-            session_time = os.time() - startTime,
-            player_count = #player.GetHumans(),
-            map = game.GetMap(),
-        }
+        doglogs_ping = pingData
     } ) )
 end
 
