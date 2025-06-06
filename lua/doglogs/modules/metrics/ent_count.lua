06352d4acdfae2bed7f83dc862f9b1d27b36ecc7
Sarah Sturgeon <sarah@sarahsturgeon.com>
2025-06-06 10:21:24 -0700

    Add constraint metric

--- lua/doglogs/modules/metrics/ent_count.lua
+++ lua/doglogs/modules/metrics/ent_count.lua
@@ -9,18 +9,22 @@ DogMetrics:NewMetric( {
 } )
 
 
+--- @type Entity
 local entMeta = assert( FindMetaTable( "Entity" ) )
 local ent_IsValid = entMeta.IsValid
 local ent_GetClass = entMeta.GetClass
 local ent_IsNPC = entMeta.IsNPC
 local ent_IsNextBot = entMeta.IsNextBot
+local ent_IsConstraint = entMeta.IsConstraint
 local ent_GetPhysicsObject = entMeta.GetPhysicsObject
+
+local interval = 10
 
 local npcCount = 0
 local NPCTracker = DogMetrics:NewMetric( {
     name = "cfc.server.entCount.npc",
     unit = "npc",
-    interval = 10,
+    interval = interval,
     metricType = DogMetrics.MetricTypes.Gauge,
 } )
 
@@ -28,7 +32,7 @@ local propPhysicsCount = 0
 local PropPhysicsTracker = DogMetrics:NewMetric( {
     name = "cfc.server.entCount.props.total",
     unit = "prop",
-    interval = 10,
+    interval = interval,
     metricType = DogMetrics.MetricTypes.Gauge,
 } )
 
@@ -36,11 +40,19 @@ local unfrozenPropCount = 0
 local UnfrozenTracker = DogMetrics:NewMetric( {
     name = "cfc.server.entCount.props.unfrozen",
     unit = "prop",
-    interval = 10,
+    interval = interval,
     metricType = DogMetrics.MetricTypes.Gauge,
 } )
 
-timer.Create( "DogMetrics_EntityCounter", 10, 0, function()
+local constraintCount = 0
+local ConstraintTracker = DogMetrics:NewMetric( {
+    name = "cfc.server.entCount.constraint",
+    unit = "constraint",
+    interval = interval,
+    metricType = DogMetrics.MetricTypes.Gauge,
+} )
+
+timer.Create( "DogMetrics_EntityCounter", interval, 0, function()
     npcCount = 0
     propPhysicsCount = 0
     unfrozenPropCount = 0
@@ -52,12 +64,16 @@ timer.Create( "DogMetrics_EntityCounter", 10, 0, function()
         local ent = allEnts[i]
         if ent_IsNPC( ent ) or ent_IsNextBot( ent ) then
             npcCount = npcCount + 1
-        elseif ent_IsValid( ent ) and ent_GetClass() == "prop_physics" then
-            propPhysicsCount = propPhysicsCount + 1
+        elseif ent_IsValid( ent ) then
+            if ent_GetClass( ent ) == "prop_physics" then
+                propPhysicsCount = propPhysicsCount + 1
 
-            local physObj = ent_GetPhysicsObject( ent )
-            if IsValid( physObj ) and not physObj:IsAsleep() then
-                unfrozenPropCount = unfrozenPropCount + 1
+                local physObj = ent_GetPhysicsObject( ent )
+                if IsValid( physObj ) and not physObj:IsAsleep() then
+                    unfrozenPropCount = unfrozenPropCount + 1
+                end
+            elseif ent_IsConstraint( ent ) then
+                constraintCount = constraintCount + 1
             end
         end
     end
@@ -65,4 +81,5 @@ timer.Create( "DogMetrics_EntityCounter", 10, 0, function()
     NPCTracker:AddPoint( npcCount )
     PropPhysicsTracker:AddPoint( propPhysicsCount )
     UnfrozenTracker:AddPoint( unfrozenPropCount )
+    ConstraintTracker:AddPoint( constraintCount )
 end )
